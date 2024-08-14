b51c8bfddb967a3840aeb0191d18e71839e2348a
atharva-gpu <atharvaphalke2105@gmail.com>
2024-08-14 12:28:16 +0530

    Create bhumika_patil.c

---
+++ bhumika_patil.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // Define the details about Bhumika Patil
+    const char *name = "Bhumika Patil";
+    const char *description = "Bhumika Patil is a software developer always eager to learn new things.";
+
+    // Print the details to the console
+    printf("Name: %s\n", name);
+    printf("Description: %s\n", description);
+
+    return 0;
+}
